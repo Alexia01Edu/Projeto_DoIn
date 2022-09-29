d59a01a177cdba285738d21e7096051702ac85c4
alexia <alexiaedu012004@gmail.com>
2022-09-29 14:50:05 -0300

    doin

---
+++ PROGRAMAS/blog-main/core/usuario_repositorio.php
@@ -0,0 +1,138 @@
+<?php
+ session_start();
+ require_once '../includes/funcoes.php';
+ require_once 'conexao_mysql.php';
+ require_once 'sql.php';
+ require_once 'mysql.php';
+ $salt = '$exemplosaltifsp';
+
+ foreach($_POST as $indice => $dado) {
+     $$indice = limparDados($dado);
+ }
+
+ foreach($_GET as $indice => $dado) {
+     $$indice = limparDados($dado);
+ }
+
+ switch($acao) {
+     case 'insert':
+         $dados = [
+            'nome' => $nome,
+            'email' => $email,
+            'senha' => crypt($senha,$salt),
+           //senha fica cripytografada
+            'cpf' => $cpf,
+            'dataNasc' => $dataNasc,
+            'quantDoar' => 0,
+            'quantTrocar' =>0
+         ];
+
+         insere(
+             'usuario',
+             $dados
+         );
+
+         break;
+     case 'update':
+         $id = (int)$id;
+         $dados = [
+             'nome' => $nome,
+             'email' => $email
+         ];
+
+         $criterio = [
+             ['id', '=', $id]
+         ];
+
+         atualiza(
+             'usuario',
+             $dados,
+             $criterio
+         );
+
+         break;
+     case 'login':
+        //se input name='login' então 
+         $criterio = [
+             ['email', '=', $email],
+             ['AND', 'ativo', '=', 1]
+         ];
+         //armazena o email na tabela usuario no campo email
+         //E coloca ativo igual a 1 na tabela do banco de dados
+
+         $retorno = buscar(
+             'usuario',
+             ['id', 'nome', 'email', 'senha', 'adm'],
+             $criterio
+         );
+         //a função buscar, verifica na tabela usuario se aquele usuario existe;
+         //compara os valores do array criterio com os valores da tabela usuario
+         //retorna 1 se sim e 0 se não;
+         if(count($retorno) > 0) {
+            //se o retorno for 1 ou seja se o usuario estiver cadastrado
+             if(crypt($senha,$salt) == $retorno[0]['senha']) {
+                //se a senha descrypitografada for igual a senha digitada
+                 $_SESSION['login']['usuario'] = $retorno[0];
+                 if(!empty($_SESSION['url_retorno'])) {
+                     header('Location: ' . $_SESSION['url_retorno']);
+                     $_SESSION['url_retorno'] = '';
+                     exit;
+                 }
+             }
+         }
+
+         break;
+     case 'logout':
+         session_destroy();
+         break;
+     case 'status':
+         $id = (int)$id;
+         $valor = (int)$valor;
+
+         $dados = [
+             'ativo' => $valor
+         ];
+
+         $criterio = [
+             ['id', '=', $id]
+         ];
+
+         atualiza(
+             'usuario',
+             $dados,
+             $criterio
+         );
+
+         header('Location: ../usuarios.php');
+         exit;
+         break;
+     case 'adm':
+         $id = (int)$id;
+         $valor = (int)$valor;
+
+         $dados = [
+             'adm' => $valor
+         ];
+         // o array dados recebe a chave(campo) adm com o valor $valor
+
+         $criterio = [
+             ['id', '=', $id]
+         ];
+         //criterio armazena o id
+
+         atualiza(
+             'usuario',
+             $dados,
+             $criterio
+         );
+         // a função atualiza a tabela usuario com os novos dados
+         //no registro $criterio
+
+         header('Location: ../usuarios.php');
+         exit;
+         break;
+ }
+ header('Location: ../index.php');
+
+ //http://localhost/blog/core/usuario_repositorio.php
+?>
---
+++ PROGRAMAS/blog-main/sql/sql_comandos.sql
@@ -0,0 +1,3 @@
+delete from blog.usuario where id=2;
+delete from blog.post where id=4;
+INSERT INTO blog.post(titulo, texto, usuario_id, data_postagem) VALUES('Como fazer pão de mel?', 'compre pão de mel, pois os ingredientes estão caros', 1, '2020-02-23 11:12:36');

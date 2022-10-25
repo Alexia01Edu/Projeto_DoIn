9579a1393470d2ecc906a3eb829f450a07ef9dea
alexia <alexiaedu012004@gmail.com>
2022-10-25 15:11:15 -0300

    doin

--- PROGRAMAS/blog-main/Ranking.php
+++ PROGRAMAS/blog-main/Ranking.php
@@ -5,41 +5,38 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Usuários | Projeto para Web com PHP</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="lib/css/Ranquing.css">
     </head>
     <body>
     
-    <div class="container-fluid">
+    <div >
         
     </div>
         
     <nav>
-        <div class="nav nav-tabs" id="nav-tab" role="tablist">
-            <a class="nav-item nav-link active" id="nav-doa-tab" data-toggle="tab" href="#nav-doa" role="tab" aria-controls="nav-home" >DOAÇÃO</a>
-            <a class="nav-item nav-link" id="nav-troca-tab" data-toggle="tab" href="#nav-troca" role="tab" aria-controls="nav-troca" >TROCA</a>
-            <a class="nav-item nav-link" id="nav-ambos-tab" data-toggle="tab" href="#nav-ambos" role="tab" aria-controls="nav-contact" >DOAÇÃO E TROCA</a>
+        <div class="menu">
+            <div class="link"><a href="#nav-doa">DOAÇÃO</a></div>
+            <div class="link"><a href="#nav-troca" >TROCA</a></div>
+            <div class="link"><a href="#nav-ambos">DOAÇÃO E TROCA</a></div>
         </div>
     </nav>
-        <div class="tab-content" id="nav-tabContent">
-            <div class="tab-pane fade container show active" id="nav-doa" role="tabpanel" aria-labelledby="nav-doa-tab">
+        <div class="" id="">
+            <div class="conteudo" id="nav-doa">
                 <?php
                     include 'includes/tabelaDoar.php';
                  ?>
             </div>
-            <div class="tab-pane fade container" id="nav-troca" role="tabpanel" aria-labelledby="nav-troca-tab">
+            <div class="conteudo" id="nav-troca" >
                 <?php
                     include 'includes/tabelaTrocar.php';
                 ?>
             </div>
-            <div class="tab-pane fade container" id="nav-ambos" role="tabpanel" aria-labelledby="nav-ambos-tab">
+            <div class="conteudo" id="nav-ambos" >
                 <?php
                     include 'includes/tabelaTrocaDoa.php';
                 ?>
             </div>
         </div>
-
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="lib/js/Ranqing.js"></script>
     </body>
 </html>
---
+++ PROGRAMAS/blog-main/lib/js/Ranqing.js
@@ -0,0 +1,7 @@
+var div = document.getElementsByClassName('conteudo');
+var linkA = document.getElementsByClassName('link');
+
+
+link.addEventListener('click', function () {
+    div.classList.toggle('display');
+});
